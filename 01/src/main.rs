f7e48dd642ef120e70d72df27cec20f41a2ec47d
Dariusz D <darekduda21@gmail.com>
2025-12-03 00:11:13 +0100

    feat(01): add solution for part 2
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -17,7 +17,7 @@ pub fn read_file(filepath: &str) -> Vec<String> {
     file_content.split("\n").map(|s| s.to_string()).collect()
 }
 
-fn load_rotations(filepath: &str, higher_bound: u64) -> Vec<Rotation> {
+fn load_rotations(filepath: &str) -> Vec<Rotation> {
     let mut rotations = Vec::new();
 
     for line in read_file(filepath) {
@@ -27,8 +27,8 @@ fn load_rotations(filepath: &str, higher_bound: u64) -> Vec<Rotation> {
 
             match num.parse::<u64>() {
                 Ok(val) => match rot.as_str() {
-                    "R" => rotations.push(Rotation::Right(val % higher_bound)),
-                    "L" => rotations.push(Rotation::Left(val % higher_bound)),
+                    "R" => rotations.push(Rotation::Right(val)),
+                    "L" => rotations.push(Rotation::Left(val)),
                     &_ => todo!(),
                 },
                 Err(_) => todo!(),
@@ -39,38 +39,50 @@ fn load_rotations(filepath: &str, higher_bound: u64) -> Vec<Rotation> {
     rotations
 }
 
-fn perform_rotation(start: u64, rotation: &Rotation, higher_bound: u64) -> u64 {
+fn perform_rotation(start: u64, rotation: &Rotation, higher_bound: u64) -> (u64, u64) {
     let lower_bound = 0;
+    let mut zeros = 0;
 
     let mut result: u64 = start;
 
     match rotation {
         Rotation::Left(val) => {
             if *val > result {
+                if result != 0 {
+                    zeros += 1;
+                }
                 result = higher_bound - (val - result);
             } else {
                 result -= val;
+                if result == 0 {
+                    zeros += 1;
+                }
             }
         }
         Rotation::Right(val) => {
             if *val + result >= higher_bound {
+                zeros += 1;
                 result = lower_bound + ((result + val) - higher_bound);
             } else {
                 result += val;
             }
         }
     }
-    result
+    (zeros, result)
 }
 
-fn part_one(rotations: &Vec<Rotation>, higher_bound: u64) {
+fn part_one(rotations: &Vec<Rotation>) {
     let start: u64 = 50;
     let higher_bound = 100;
 
     let mut res = start;
     let mut zeros = 0;
     for rotation in rotations {
-        res = perform_rotation(res, rotation, higher_bound);
+        let rot = match rotation {
+            Rotation::Left(val) => &Rotation::Left(val % higher_bound),
+            Rotation::Right(val) => &Rotation::Right(val % higher_bound),
+        };
+        (_, res) = perform_rotation(res, rot, higher_bound);
         println!("{} {:?}", res, rotation);
         if res == 0 {
             zeros += 1;
@@ -79,8 +91,44 @@ fn part_one(rotations: &Vec<Rotation>, higher_bound: u64) {
     println!("Number of zeros: {}", zeros);
 }
 
-fn main() {
+fn part_two(rotations: &Vec<Rotation>) {
+    let start: u64 = 50;
     let higher_bound = 100;
-    let rotations = load_rotations("input", higher_bound);
-    part_one(&rotations, higher_bound);
+
+    let mut res = start;
+    let mut zeros = 0;
+    for rotation in rotations {
+        let rot = match rotation {
+            Rotation::Left(val) => {
+                let div = val / higher_bound;
+                if div > 0 {
+                    zeros += div;
+                }
+                &Rotation::Left(val % higher_bound)
+            }
+            Rotation::Right(val) => {
+                let div = val / higher_bound;
+                if div > 0 {
+                    zeros += div;
+                }
+
+                &Rotation::Right(val % higher_bound)
+            }
+        };
+        let zer;
+        (zer, res) = perform_rotation(res, rot, higher_bound);
+        zeros += zer;
+        println!("{} {} {:?}", res, zer, rotation);
+    }
+    println!("Number of zeros: {}", zeros);
+}
+
+fn main() {
+    let rotations = load_rotations("input_test");
+    part_one(&rotations);
+    part_two(&rotations);
+
+    let rotations = load_rotations("input");
+    part_one(&rotations);
+    part_two(&rotations);
 }
