daa77bb32528a0852bc502f3ea74e69f81b0dc70
Dariusz D <darekduda21@gmail.com>
2025-12-07 17:04:15 +0100

    feat(03): add solution for part 2
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -43,6 +43,43 @@ fn find_highest_joltage(banks: Vec<Vec<u64>>) -> u64 {
     res
 }
 
+fn find_highest_joltage_12_batteries(banks: Vec<Vec<u64>>) -> u64 {
+    let mut res = 0;
+
+    for bank in banks {
+        let mut numbers = 0;
+        let mut num_str = String::new();
+        let mut last_max_idx = 0;
+        let bank_size = bank.len();
+        let mut slice = &bank[0..(bank_size - (12 - numbers))];
+        while numbers < 12 {
+            if let Some(max) = slice.iter().max() {
+                if let Some(index) = slice.iter().position(|x| x == max) {
+                    num_str.push_str(&format!("{}", max));
+                    last_max_idx = last_max_idx + index + 1;
+                    numbers += 1;
+                    slice = if numbers != 12 {
+                        &bank[last_max_idx..(bank_size - (12 - numbers) + 1)]
+                    } else {
+                        &bank[last_max_idx..]
+                    };
+                } else {
+                    panic!("Cannot find max {}", max);
+                }
+            }
+        }
+        match num_str.parse::<u64>() {
+            Ok(val) => {
+                // println!("Res: {}", num_str);
+                res += val;
+            }
+            Err(_) => panic!("Cannot parse {}", num_str),
+        }
+    }
+
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -54,9 +91,18 @@ mod tests {
     }
 
     #[test]
-    fn is_part2_working_with_test_input() {}
+    fn is_part2_working_with_test_input() {
+        let banks = read_banks("input_test");
+        assert_eq!(find_highest_joltage_12_batteries(banks), 3121910778619);
+    }
 }
 fn main() {
     let banks = read_banks("day03/input");
     println!("Total output is: {}", find_highest_joltage(banks));
+
+    let banks = read_banks("day03/input");
+    println!(
+        "Total output is: {}",
+        find_highest_joltage_12_batteries(banks)
+    );
 }
