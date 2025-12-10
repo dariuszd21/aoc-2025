218742d6c69029fb59a7a4438a2e4a442bd8526d
Dariusz D <darekduda21@gmail.com>
2025-12-10 21:04:06 +0100

    feat(05): add solution for part 2
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,4 +1,5 @@
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
+
 struct Range {
     lower_bound: u64,
     upper_bound: u64,
@@ -14,6 +15,56 @@ fn find_fresh(ranges: Vec<Range>, ingredients: Vec<u64>) -> u64 {
                 break;
             }
         }
+    }
+
+    res
+}
+
+fn combine_ranges(ranges: Vec<Range>) -> Vec<Range> {
+    let mut sorted_ranges: Vec<_> = ranges.clone();
+    sorted_ranges.sort_by(|a, b| a.lower_bound.cmp(&b.lower_bound));
+
+    // invert logic - add ranges one-by-one by either extending the previous ones or creating a new
+    // entry
+
+    let mut combined_ranges: Vec<Range> = Vec::new();
+    for range in &sorted_ranges {
+        let mut insert_new = true;
+        // println!("Analysing: {:?}", range);
+        for combined_range in &mut combined_ranges {
+            // println!("Combined: {:?}", combined_range);
+            if combined_range.lower_bound <= range.lower_bound
+                && range.lower_bound <= combined_range.upper_bound
+            {
+                let max_upper = if range.upper_bound > combined_range.upper_bound {
+                    range.upper_bound
+                } else {
+                    combined_range.upper_bound
+                };
+
+                *combined_range = Range {
+                    lower_bound: combined_range.lower_bound,
+                    upper_bound: max_upper,
+                };
+                // println!("Found {:?}", combined_range);
+                insert_new = false;
+                break;
+            }
+        }
+        if insert_new {
+            // println!("Inserting: {:?}", range);
+            combined_ranges.push(range.clone());
+        }
+    }
+
+    combined_ranges
+}
+
+fn count_fresh_ids(ranges: Vec<Range>) -> u64 {
+    let mut res = 0;
+
+    for range in combine_ranges(ranges) {
+        res += (range.upper_bound - range.lower_bound) + 1;
     }
 
     res
@@ -64,10 +115,15 @@ mod tests {
     }
 
     #[test]
-    fn is_part2_working_with_test_input() {}
+    fn is_part2_working_with_test_input() {
+        let (ranges, _) = load_input("input_test");
+        assert_eq!(count_fresh_ids(ranges), 14);
+    }
 }
 
 fn main() {
     let (ranges, ingredients) = load_input("day05/input");
     println!("Result: {}", find_fresh(ranges, ingredients));
+    let (ranges, _) = load_input("day05/input");
+    println!("Result: {}", count_fresh_ids(ranges));
 }
